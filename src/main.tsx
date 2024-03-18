2c0df9962e9319994ed3bfbfaad26a93b110782b
meik1998news <mohammad+test@newsifier.com>
2024-03-18 13:56:11 +0300

    Wrap main App with new contexts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { FilterProvider } from "./contexts/filterContext.tsx";
+import { NewsProvider } from "./contexts/newsListContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <FilterProvider>
+      <NewsProvider>
+        <App />
+      </NewsProvider>
+    </FilterProvider>
+  </React.StrictMode>
+);

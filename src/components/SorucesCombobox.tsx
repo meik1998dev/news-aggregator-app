bd491cefa48f23a736fe843a2960ae2a59dc138f
meik1998news <mohammad+test@newsifier.com>
2024-03-19 18:20:18 +0300

    Create sourceCombobox

---
+++ src/components/SorucesCombobox.tsx
@@ -0,0 +1,27 @@
+import { Combobox } from "./common/combobox";
+import { useFilters } from "@/contexts/filterContext";
+import { useSourcesItems } from "@/contexts/sourcesContext";
+import { useState } from "react";
+
+export const SorucesCombobox = () => {
+  const { sourcesItems } = useSourcesItems();
+  const { filters, setFilters } = useFilters();
+  const [sourcesArr, setSourcesArr] = useState<Array<string>>([]);
+
+  const handleOpenAction = () => {
+    setSourcesArr([...sourcesItems]);
+  };
+
+  const handleSelect = (value: string) => {
+    setFilters({ ...filters, source: value });
+  };
+
+  return (
+    <Combobox
+      handleSelect={handleSelect}
+      type={"source"}
+      options={sourcesArr}
+      onOpenAction={handleOpenAction}
+    />
+  );
+};
